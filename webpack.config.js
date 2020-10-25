32757cba888ada911cc5225b321bee0d709dcf62
Craig Walker <github@softcraft.ca>
2020-10-24 19:42:20 -0600

    webpack sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
-/* global module */
+/* global module, require */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
 module.exports = {
   entry: "./src/index.ts",
+  devtool: "source-map",
   module: {
     rules: [
       {
@@ -8,8 +13,34 @@ module.exports = {
         use: "ts-loader",
         exclude: /node_modules/,
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
     ],
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
   },
