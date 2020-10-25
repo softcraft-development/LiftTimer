6d4d91ad4d3c6850edbfcc48275cae3aab3fd982
Craig Walker <github@softcraft.ca>
2020-10-24 19:16:50 -0600

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+/* global module */
+module.exports = {
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+}
